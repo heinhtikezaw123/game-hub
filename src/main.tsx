292aa3f7c2c8b6b7527b4525a6e6d3a719a420d1
heinhtike123 <heinhtikezaw2021hh@gmail.com>
2026-05-20 20:01:43 -0400

    Installed Chakra Properly

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+// import { Provider } from "@/components/ui/provider";
+import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-)
+);
