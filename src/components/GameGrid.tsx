dce94eed0692fbc9ae857153fc793274b31a7811
heinhtike123 <heinhtikezaw2021hh@gmail.com>
2026-06-21 15:16:03 -0400

    Refector Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,9 +11,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  if(error) return <Text textColor="red">{error}</Text>
   return (
-    <div>
-      {error && <Text textColor="red">{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -32,7 +32,6 @@ const GameGrid = ({ gameQuery }: Props) => {
               </GameCardContainer>
             ))}
       </SimpleGrid>
-    </div>
   );
 };
 
